1cc05d308f120c25b1615d9f2dfa5bd921dfea21
Ben Tran <46247492+BenOnSocial@users.noreply.github.com>
2022-11-15 19:29:14 -0800

    Refactor text generation test to be reusable with multiple models.

--- tests/test_text_generation.py
+++ tests/test_text_generation.py
@@ -1,14 +1,28 @@
 import datetime
 import os
-import sys
 import time
+from enum import Enum, unique
+from typing import Callable, Dict, List
 
 import pytest
 from transformers import pipeline, set_seed
 
 
+# TODO: Switch to StrEnum when PyTorch supports Python 3.11.
+@unique
+class Model(str, Enum):
+    GPT2_LARGE = "gpt2-large"
+    GTP_NEO_2_7B = "EleutherAI/gpt-neo-2.7B"
+
+    def __str__(self) -> str:
+        return self.value
+
+
 class TestTextGeneration:
     output_dir: str = "build/test/TestTextGeneration"
+
+    def __int__(self) -> None:
+        self.start_time: float = 0.0
 
     @classmethod
     def setup_class(cls) -> None:
@@ -17,29 +31,32 @@ class TestTextGeneration:
         except OSError:
             pass
 
+    def setup_method(self, method: Callable) -> None:
+        self.start_time = time.time()
+        set_seed(int(self.start_time))
+
     @pytest.mark.parametrize(
-        "input_text",
+        "name, input_text, model",
         [
-            "why is local SEO important for small business?",
-            "leveraging email marketing in small business",
-            "what's the best electric car to purchase in 2022?",
-            "why is machine learning so important for modern technology",
+            ("local-seo", "why is local SEO important for small business?", Model.GPT2_LARGE),
+            ("local-seo", "why is local SEO important for small business?", Model.GTP_NEO_2_7B),
+            ("email-marketing", "leveraging email marketing in small business", Model.GPT2_LARGE),
+            ("email-marketing", "leveraging email marketing in small business", Model.GTP_NEO_2_7B),
+            ("2022-electric-cars", "what's the best electric car to purchase in 2022?", Model.GPT2_LARGE),
+            ("2022-electric-cars", "what's the best electric car to purchase in 2022?", Model.GTP_NEO_2_7B),
+            ("machine-learning", "why is machine learning so important in modern technology", Model.GPT2_LARGE),
+            ("machine-learning", "why is machine learning so important in modern technology", Model.GTP_NEO_2_7B),
         ],
     )
-    def test_gpt2_large(self, input_text: str) -> None:
-        start_time: float = time.time()
+    def test_long_form_text_generation(self, name: str, input_text: str, model: Model) -> None:
+        generator = pipeline(task="text-generation", model=model)
+        output = generator(input_text, min_length=100, max_length=500, num_return_sequences=5)
 
-        set_seed(int(start_time))
+        self._write_output(name=name, model=model, output=output, end_time=time.time())
 
-        generator = pipeline(task="text-generation", model="gpt2-large")
-        output = generator(text_inputs=input_text, min_length=500, max_length=1000, num_return_sequences=5)
-
-        end_time: float = time.time()
-
-        with open(
-            f"{TestTextGeneration.output_dir}/{sys._getframe().f_code.co_name}-{int(end_time)}.json", "a"
-        ) as file:
+    def _write_output(self, name: str, model: Model, output: List[Dict], end_time: float) -> None:
+        with open(f"{TestTextGeneration.output_dir}/{name}-{model.name}-{int(end_time)}.txt", "a") as file:
             for generated_text in [result["generated_text"] for result in output]:
                 file.write(generated_text)
                 file.write("\n\n==========\n\n")
-            file.write(f">>>> Run time: {str(datetime.timedelta(seconds=end_time - start_time))}\n\n\n")
+            file.write(f">>>> Run time: {str(datetime.timedelta(seconds=end_time - self.start_time))}\n\n\n")
